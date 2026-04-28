cbf7250cfe181d53cd2a050d0b547a8ffc01b8d9
Shaik Althaf <23071a12c0@vnrvjiet.in>
2026-04-28 07:33:27 +0530

    added week-6

---
+++ decisiontree.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeClassifier(criterion='entropy')
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Correct Predictions:")
+for i in range(len(y_test)):
+    if y_test[i] == y_pred[i]:
+        print("Actual:", y_test[i], "Predicted:", y_pred[i])
+
+print("\nIncorrect Predictions:")
+for i in range(len(y_test)):
+    if y_test[i] != y_pred[i]:
+        print("Actual:", y_test[i], "Predicted:", y_pred[i])
+
+sample = [X_test[0]]
+prediction = model.predict(sample)
+
+print("\nNew Sample Prediction:")
+print("Sample:", sample)
+print("Predicted Class:", prediction[0])
