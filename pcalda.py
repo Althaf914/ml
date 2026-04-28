71399acd52a2e0652bdfa681139b2892082926c9
Shaik Althaf <23071a12c0@vnrvjiet.in>
2026-04-28 07:34:46 +0530

    added week-8

---
+++ pcalda.py
@@ -0,0 +1,29 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train)
+X_test_pca = pca.transform(X_test)
+
+print("PCA Transformed Train Data:")
+print(X_train_pca[:5])
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_train_lda = lda.fit_transform(X_train, y_train)
+X_test_lda = lda.transform(X_test)
+
+print("\nLDA Transformed Train Data:")
+print(X_train_lda[:5])
