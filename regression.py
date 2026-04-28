a2c80484bc8227285e103c76eefbf5d38839b813
Shaik Althaf <23071a12c0@vnrvjiet.in>
2026-04-28 12:11:08 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -3,6 +3,7 @@ from sklearn.datasets import load_diabetes
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 data = load_diabetes()
 X = data.data
@@ -22,6 +23,13 @@ print(r2_score(y_test_s, y_pred_s))
 print(model_simple.coef_)
 print(model_simple.intercept_)
 
+plt.scatter(X_test_s, y_test_s)
+plt.plot(X_test_s, y_pred_s)
+plt.title("Simple Linear Regression")
+plt.xlabel("Feature")
+plt.ylabel("Target")
+plt.show()
+
 X_train_m, X_test_m, y_train_m, y_test_m = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model_multiple = LinearRegression()
@@ -33,3 +41,9 @@ print(mean_squared_error(y_test_m, y_pred_m))
 print(r2_score(y_test_m, y_pred_m))
 print(model_multiple.coef_)
 print(model_multiple.intercept_)
+
+plt.scatter(y_test_m, y_pred_m)
+plt.title("Multiple Linear Regression")
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
+plt.show()
